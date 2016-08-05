5bab3a5c51beabae032cdcda19e83ba53ebb1f76
kamil <kamil@socialcee.com>
2016-08-05 19:41:36 +0200

    initial commit

---
+++ tests/WebSharper.AmplifyJS.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module WebSharper.AmplifyJS.Tests
+
+open WebSharper.AmplifyJS
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
