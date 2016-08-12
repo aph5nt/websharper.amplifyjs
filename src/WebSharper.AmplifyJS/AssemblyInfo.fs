55c4b23cc753b7f9d38f1497aea2aa11e79556b3
kamil <kamil@socialcee.com>
2016-08-12 22:15:54 +0200

    Bump version to 0.1.0.2

--- src/WebSharper.AmplifyJS/AssemblyInfo.fs
+++ src/WebSharper.AmplifyJS/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("WebSharper.AmplifyJS")>]
 [<assembly: AssemblyProductAttribute("WebSharper.AmplifyJS")>]
 [<assembly: AssemblyDescriptionAttribute("Project has no summmary; update build.fsx")>]
-[<assembly: AssemblyVersionAttribute("0.1.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.1.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0.1"
-    let [<Literal>] InformationalVersion = "0.1.0.1"
+    let [<Literal>] Version = "0.1.0.2"
+    let [<Literal>] InformationalVersion = "0.1.0.2"
